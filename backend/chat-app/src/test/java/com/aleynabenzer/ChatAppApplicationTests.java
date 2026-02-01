44f560cfe27136368ff6b300b70bd73b01361d12
Aleyna_B <benz.aleyna@gmail.com>
2026-02-01 03:50:42 +0300

    fix import

--- backend/chat-app/src/test/java/com/aleynabenzer/ChatAppApplicationTests.java
+++ backend/chat-app/src/test/java/com/aleynabenzer/ChatAppApplicationTests.java
@@ -2,6 +2,7 @@ package com.aleynabenzer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.aleynabenzer.chat.app.ChatAppApplication;
 
 @SpringBootTest(classes = ChatAppApplication.class)	//expicitly specify the application class bc the package names do not match
 class ChatAppApplicationTests {
