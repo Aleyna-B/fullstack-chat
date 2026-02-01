b4638e442a3366104768ffb64d1697aedd2d46b0
Aleyna_B <benz.aleyna@gmail.com>
2026-02-01 03:42:40 +0300

    main and test package names do not match, fix expilicitly with annotation

--- backend/chat-app/src/test/java/com/aleynabenzer/ChatAppApplicationTests.java
+++ backend/chat-app/src/test/java/com/aleynabenzer/ChatAppApplicationTests.java
@@ -3,7 +3,7 @@ package com.aleynabenzer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ChatAppApplication.class)	//expicitly specify the application class bc the package names do not match
 class ChatAppApplicationTests {
 
 	@Test
